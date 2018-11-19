7279d6b62a7534ba5224bbde9530f5e6db80ea38
tutkuince <tutku.ince@outlook.com>
2018-11-19 20:39:25 +0300

    added @Controller and @Autowired annotation

--- src/main/java/com/muditasoft/didemo/controllers/SetterInjectedController.java
+++ src/main/java/com/muditasoft/didemo/controllers/SetterInjectedController.java
@@ -1,8 +1,13 @@
 package com.muditasoft.didemo.controllers;
 
 import com.muditasoft.didemo.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class SetterInjectedController {
+
+    @Autowired
     private GreetingService greetingService;
 
     public String sayHello() {
