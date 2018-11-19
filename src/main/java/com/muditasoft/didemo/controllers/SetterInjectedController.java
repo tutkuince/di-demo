3d5de25239c491a9cc59a5ae14269e90f232377c
tutkuince <tutku.ince@outlook.com>
2018-11-19 20:00:15 +0300

    SetterInjectedController sample

---
+++ src/main/java/com/muditasoft/didemo/controllers/SetterInjectedController.java
@@ -0,0 +1,15 @@
+package com.muditasoft.didemo.controllers;
+
+import com.muditasoft.didemo.services.GreetingService;
+
+public class SetterInjectedController {
+    private GreetingService greetingService;
+
+    String sayHello() {
+        return greetingService.sayGreeting();
+    }
+
+    public void setGreetingService(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+}
