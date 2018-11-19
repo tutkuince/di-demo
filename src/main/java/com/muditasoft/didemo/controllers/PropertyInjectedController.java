ea26657e1c3086a79a8aba195e78bbca6b0e3799
tutkuince <tutku.ince@outlook.com>
2018-11-19 21:05:12 +0300

    GreetingServiceImpl to GreetingService injection with using @Qualifier

--- src/main/java/com/muditasoft/didemo/controllers/PropertyInjectedController.java
+++ src/main/java/com/muditasoft/didemo/controllers/PropertyInjectedController.java
@@ -1,14 +1,16 @@
 package com.muditasoft.didemo.controllers;
 
-import com.muditasoft.didemo.services.GreetingServiceImpl;
+import com.muditasoft.didemo.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PropertyInjectedController {
 
     @Autowired
-    public GreetingServiceImpl greetingService;
+    @Qualifier("greetingServiceImpl")
+    public GreetingService greetingService;
 
     public String sayHello(){
         return greetingService.sayGreeting();
