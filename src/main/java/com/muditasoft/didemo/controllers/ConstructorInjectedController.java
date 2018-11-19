7a5fd31d91be1b73e1c92e563d3b3973628b63a9
tutkuince <tutku.ince@outlook.com>
2018-11-19 20:54:58 +0300

    Annotation of GreetingService field modified Autowired to Qualifier

--- src/main/java/com/muditasoft/didemo/controllers/ConstructorInjectedController.java
+++ src/main/java/com/muditasoft/didemo/controllers/ConstructorInjectedController.java
@@ -1,13 +1,13 @@
 package com.muditasoft.didemo.controllers;
 
 import com.muditasoft.didemo.services.GreetingService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ConstructorInjectedController {
 
-    @Autowired
+    @Qualifier("constructorGreetingService")
     private GreetingService greetingService;
 
     public ConstructorInjectedController(GreetingService greetingService) {
