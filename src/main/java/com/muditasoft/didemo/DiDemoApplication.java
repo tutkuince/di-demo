2ddaf3fd5c85a6ebf77a1ee76d302b1d46dd1141
tutkuince <tutku.ince@outlook.com>
2018-11-19 20:42:30 +0300

    PropertyInjectedController, SetterInjectedController, ConstructorInjectedController getBean

--- src/main/java/com/muditasoft/didemo/DiDemoApplication.java
+++ src/main/java/com/muditasoft/didemo/DiDemoApplication.java
@@ -1,6 +1,9 @@
 package com.muditasoft.didemo;
 
+import com.muditasoft.didemo.controllers.ConstructorInjectedController;
 import com.muditasoft.didemo.controllers.MyController;
+import com.muditasoft.didemo.controllers.PropertyInjectedController;
+import com.muditasoft.didemo.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,6 +16,12 @@ public class DiDemoApplication {
 
         MyController controller = (MyController) ctx.getBean("myController");
         controller.hello();
+
+        System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
+        System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
+        System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+
+
 
     }
 }
