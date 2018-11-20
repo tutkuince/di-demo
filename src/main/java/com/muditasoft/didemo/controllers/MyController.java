be258a19ee873970ed8764ee45f8fbc685e7da3b
tutkuince <tutku.ince@outlook.com>
2018-11-20 17:09:38 +0300

    GreetingService injected

--- src/main/java/com/muditasoft/didemo/controllers/MyController.java
+++ src/main/java/com/muditasoft/didemo/controllers/MyController.java
@@ -1,11 +1,18 @@
 package com.muditasoft.didemo.controllers;
 
+import com.muditasoft.didemo.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
-    public String hello(){
+    private GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String hello() {
         System.out.println("Helloo!!!");
         return "foo";
     }
