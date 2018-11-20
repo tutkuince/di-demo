8562fbd5a9be89b10919906fab309f6dc381aa46
tutkuince <tutku.ince@outlook.com>
2018-11-20 17:14:40 +0300

    HELLO_EVERYONE = "Hello Everyone!!!" to HELLO_EVERYONE = "Hello Everyone!!! - Original";

--- src/main/java/com/muditasoft/didemo/services/GreetingServiceImpl.java
+++ src/main/java/com/muditasoft/didemo/services/GreetingServiceImpl.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingServiceImpl implements GreetingService {
-    public static final String HELLO_EVERYONE = "Hello Everyone!!!";
+    public static final String HELLO_EVERYONE = "Hello Everyone!!! - Original";
 
     @Override
     public String sayGreeting() {
