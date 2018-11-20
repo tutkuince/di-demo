cdc17dc437acf56ac0f15cae044e7ab81c5ea107
tutkuince <tutku.ince@outlook.com>
2018-11-20 18:02:38 +0300

    Added @Profile("en")

--- src/main/java/com/muditasoft/didemo/services/PrimaryGreetingService.java
+++ src/main/java/com/muditasoft/didemo/services/PrimaryGreetingService.java
@@ -1,10 +1,12 @@
 package com.muditasoft.didemo.services;
 
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
 @Primary
+@Profile("en")
 public class PrimaryGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
