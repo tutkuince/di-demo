9be6a742cfba55bec2dbef999bb1f38871736a36
tutkuince <tutku.ince@outlook.com>
2018-11-20 17:09:53 +0300

    @Primary annotation added

--- src/main/java/com/muditasoft/didemo/services/PrimaryGreetingService.java
+++ src/main/java/com/muditasoft/didemo/services/PrimaryGreetingService.java
@@ -1,8 +1,10 @@
 package com.muditasoft.didemo.services;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service
+@Primary
 public class PrimaryGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
