dd5f8d083cb7ec1037de5e64180987e1e4206cfa
tutkuince <tutku.ince@outlook.com>
2018-11-20 18:00:50 +0300

    @Primary to PrimaryTurkishGreetingService

--- src/main/java/com/muditasoft/didemo/services/PrimaryTurkishGreetingService.java
+++ src/main/java/com/muditasoft/didemo/services/PrimaryTurkishGreetingService.java
@@ -1,10 +1,12 @@
 package com.muditasoft.didemo.services;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
 @Profile("tr")
+@Primary
 public class PrimaryTurkishGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
