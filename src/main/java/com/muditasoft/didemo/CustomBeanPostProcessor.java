3c42cbf15bc29237b13a97a03817c29781b95025
tutkuince <tutku.ince@outlook.com>
2018-11-21 13:30:47 +0300

    Created CustomBeanPostProcessor and implements BeanPostProcessor

---
+++ src/main/java/com/muditasoft/didemo/CustomBeanPostProcessor.java
@@ -0,0 +1,23 @@
+package com.muditasoft.didemo;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomBeanPostProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        if (bean instanceof LifeCycleBeanDemo)
+            ((LifeCycleBeanDemo) bean).beforeInit();
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if (bean instanceof LifeCycleBeanDemo) {
+            ((LifeCycleBeanDemo) bean).afterInit();
+        }
+        return bean;
+    }
+}
